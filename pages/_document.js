4d2bc6c3c625039561c4937840ff3c785b882fac
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-05-04 07:49:41 +0800

    done apply fonts in Document

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link ref="preload" href="/fonts/Poppins-Regular.ttf" as="font" crossOrigin='anonymous'></link>
+      <link ref="preload" href="/fonts/Poppins-SemiBold.ttf" as="font" crossOrigin='anonymous'></link>
+      <link ref="preload" href="/fonts/Poppins-Bold.ttf" as="font" crossOrigin='anonymous'></link>
       <body>
         <Main />
         <NextScript />
