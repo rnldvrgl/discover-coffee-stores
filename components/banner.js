a206cca3941c088aeb593bf87d9a196a269858ef
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-03 16:06:34 +0800

    done style our homepage

--- components/banner.js
+++ components/banner.js
@@ -4,11 +4,15 @@ const Banner = (props) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>
-                <span className={styles.title1}>Coffee</span> 
+                <span className={styles.title1}>Coffee</span>
                 <span className={styles.title2}>Connoisseuer</span>
             </h1>
             <p className={styles.subTitle}>Discover your local coffee shops</p>
-            <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+            <div className={styles.buttonWrapper}>
+                <button className={styles.button} onClick={props.handleOnClick}>
+                    {props.buttonText}
+                </button>
+            </div>
         </div>
     )
 }
