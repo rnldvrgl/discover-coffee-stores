b4875eebb36eb5d47266bb13065e0f662b542270
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-03 11:29:09 +0800

    done Banner Component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
+import Banner from "@/components/banner"
 
 export default function Home() {
+
+  // Handle Button Function
+  const handleOnBannerBtnClick = () => {
+    console.log("hi");
+  }
+
   return (
-<div className={styles.container}>
-  <Head>
-    <title>Create Next App</title>
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  <main className={styles.main}>
-    <h1 className={styles.title}>Coffee Connoisseuer</h1>
-  </main>
+      <main className={styles.main}>
+        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+      </main>
 
-</div>
+    </div>
   )
 }
