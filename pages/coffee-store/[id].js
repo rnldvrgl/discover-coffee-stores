fce984be7ba93dc7e4acef93f60942bda72e7e44
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-03 15:33:35 +0800

    done add route using link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,14 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const CoffeeStore = () => {
     const router = useRouter();
-    return <div>Coffee Store Page</div>
+    return (
+        <>
+        <div>Coffee Store Page</div>
+        <Link href="/">Back to Home</Link>
+        </>
+    )
 }
 
 export default CoffeeStore;
