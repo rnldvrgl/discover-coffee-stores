e1bebd21306e160c0f80016271574941cab4cc39
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-03 13:25:35 +0800

    done banner head styling

--- components/banner.js
+++ components/banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => {
                 <span className={styles.title1}>Coffee</span> 
                 <span className={styles.title2}>Connoisseuer</span>
             </h1>
-            <p className={styles.subtitle}>Discover your local coffee shops</p>
+            <p className={styles.subTitle}>Discover your local coffee shops</p>
             <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
         </div>
     )
