649b9e254b0d17492ed0cd00f919a515e55e8299
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-19 17:30:04 +0800

    done fetch api asynch wait

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import coffeeStoresData from "../data/coffee-stores.json"
 // Get Static Props
 export async function getStaticProps(context) {
 
+  // API from Four Square
   const options = {
     method: 'GET',
     headers: {
@@ -16,14 +17,16 @@ export async function getStaticProps(context) {
     }
   };
 
-  fetch('https://api.foursquare.com/v3/places/search?query=coffee%20stores&ll=15.24%2C120.59&limit=6', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  // Add Await if the Response returns Promise
+  const response = await fetch('https://api.foursquare.com/v3/places/search?query=coffee%20stores&ll=15.24%2C120.59&limit=6', options);
+
+  const data = await response.json();
+
+  // .catch (err => console.error(err));
 
   return {
     props: {
-      coffeeStores: coffeeStoresData,
+      coffeeStores: data.results,
     }, //will be passed to the page component as props
   }
 }
@@ -54,7 +57,7 @@ export default function Home(props) {
             <div className={styles.cardLayout}>
               {props.coffeeStores.map(coffeeStore => {
                 return (
-                  <Card key={coffeeStore.id} name={coffeeStore.name} imgUrl={coffeeStore.imgUrl} href={`/coffee-store/${coffeeStore.id}`} className={styles.card} />
+                  <Card key={coffeeStore.id} name={coffeeStore.name} imgUrl={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80'} href={`/coffee-store/${coffeeStore.id}`} className={styles.card} />
                 )
               })}
             </div>
