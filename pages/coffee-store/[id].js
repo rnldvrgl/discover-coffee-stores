54abd8c46047e724dce12cc49996366a3fb16b38
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-05-19 22:43:01 +0800

    done update coffee store library data

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -42,7 +42,7 @@ const CoffeeStore = (props) => {
         return <div>Loading</div>
     }
 
-    const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+    const { location, name, imgUrl, related_places } = props.coffeeStore;
 
     const handleUpvoteButton = () => {
         console.log("Upvote")
@@ -65,11 +65,11 @@ const CoffeeStore = (props) => {
             <div className={cls("glass", styles.col2)}>
                 <div className={styles.iconWrapper}>
                     <Image src="/static/icons/places.svg" width="24" height="24" alt="" />
-                    <p className={styles.text}>{address}</p>
+                    <p className={styles.text}>{location.formatted_address}</p>
                 </div>
                 <div className={styles.iconWrapper}>
                     <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="" />
-                    <p className={styles.text}>{neighbourhood}</p>
+                    <p className={styles.text}>{related_places.parent.name}</p>
                 </div>
                 <div className={styles.iconWrapper}>
                     <Image src="/static/icons/star.svg" width="24" height="24" alt="" />
