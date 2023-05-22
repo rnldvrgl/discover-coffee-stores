3866e4c141020be2c29f770c005549910d40e7c7
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-22 16:47:30 +0800

    done Display Coffee Stores by Location on the Page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Card from "@/components/card"
 import Image from "next/image"
 import { fetchCoffeeStores } from "@/lib/coffee-store"
 import useTrackLocation from "@/hooks/use-track-location"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 // Get Static Props
 export async function getStaticProps(context) {
@@ -24,6 +24,9 @@ export default function Home(props) {
 
   const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } = useTrackLocation();
 
+  const [coffeeStores, setCoffeeStores] = useState('');
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
   console.log({ latLong, locationErrorMsg });
 
   useEffect(() => {
@@ -32,10 +35,12 @@ export default function Home(props) {
         try {
           const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
           console.log({ fetchedCoffeeStores });
+          setCoffeeStores(fetchedCoffeeStores);
           //set coffee stores
         } catch (error) {
           //set error
           console.log("Error", { error });
+          setCoffeeStoresError(error.message);
         }
       }
     }
@@ -59,10 +64,31 @@ export default function Home(props) {
       <main className={styles.main}>
         <Banner buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
           handleOnClick={handleOnBannerBtnClick} />
+        {/* Location Error Message */}
         {locationErrorMsg && <p>Something went wrong: {locationErrorMsg} </p>}
+
+        {/* Coffee Stores Error Message */}
+        {coffeeStoresError && <p>Something went wrong: {coffeeStoresError} </p>}
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" alt="Hero Image" width={700} height={400} />
         </div>
+
+        {/* Stores Near Me */}
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores Near Me:</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map(coffeeStore => {
+                return (
+                  <Card key={coffeeStore.id} name={coffeeStore.name} imgUrl={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80'} href={`/coffee-store/${coffeeStore.id}`} className={styles.card} />
+                )
+              })}
+            </div>
+          </div>
+        )}
+
+
+        {/* Coffee Stores in Mabalacat */}
         {props.coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Mabalacat Stores</h2>
