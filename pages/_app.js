191ee0dc2ef78090a3a20163823a421cdf969cdb
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-05-23 18:00:19 +0800

    done Create Store Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '@/styles/globals.css'
+import { createContext } from 'react'
+
+// Store Context
+const StoreContext = createContext();
+
+// Store Provider
+const StoreProvider = ({ children }) => {
+  // Initialize State
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  }
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider >
+  )
+}
 
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider >
   )
 }
