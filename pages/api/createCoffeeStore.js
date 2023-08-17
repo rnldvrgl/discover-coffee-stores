8cb14ddf4f50caace5e0fe276165d2dba4d77ee9
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-08-17 09:31:39 +0800

    Update createCoffeeStore.js

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,18 @@
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_TOKEN }).base(process.env.AIRTABLE_BASE_KEY);
 
+
 const table = base('coffee-stores');
+
+console.log(table)
+
+const createCoffeeStore = (req, res) => {
+
+    if (req.method == 'POST') {
+        res.json({ message: "POST" });
+    } else if (req.method == 'GET') {
+        res.json({ message: "GET" });
+    }
+}
+
+export default createCoffeeStore;
