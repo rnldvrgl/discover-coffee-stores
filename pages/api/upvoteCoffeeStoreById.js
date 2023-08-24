ec1c4d30e007b45d2e11f889b3b8b49b27ed6291
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-08-24 17:36:08 +0800

    Update upvoteCoffeeStoreById.js

--- pages/api/upvoteCoffeeStoreById.js
+++ pages/api/upvoteCoffeeStoreById.js
@@ -9,6 +9,13 @@ const upvoteCoffeeStoreById = async (req, res) => {
                 const records = await findRecordByFilter(id);
 
                 if (records.length !== 0) {
+
+                    const record = records[0];
+
+                    const calculateVoting = parseInt(record.voting) + 1;
+
+                    console.log(calculateVoting)
+
                     res.json(records);
                 } else {
                     res.json({ message: "Coffee Store doesn't exist", id })
